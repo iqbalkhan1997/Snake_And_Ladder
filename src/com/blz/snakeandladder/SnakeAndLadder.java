e4c4c2902fff3457bcdfd7422fe72baf1956e42a
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-23 15:28:15 +0530

    Added: player position should not be below 0 functionality

--- src/com/blz/snakeandladder/SnakeAndLadder.java
+++ src/com/blz/snakeandladder/SnakeAndLadder.java
@@ -1,30 +1,37 @@
 package com.blz.snakeandladder;
 
 public class SnakeAndLadder {
-      //  public static final int START_POSITIION=0;
+    public static final int START_POSITIION=0;
     public static final int IS_SNAKE=1;
     public static final int IS_LADDER=2;
+    public static final int WINNING_POSITION=100;
 
     public static void main(String[] args) {
             System.out.println("Welcome to Snake and Ladder Game");
             int currentPosition=0;
 
-        System.out.println("Player is at "+currentPosition+" position");
-        int diceNum=(int)(Math.random() * 10) % 6 + 1;
-        System.out.println("die value is "+diceNum);
-        int option=(int)(Math.random() * 10) % 3;
+        while(currentPosition<WINNING_POSITION) {
 
-        if(option==IS_SNAKE){
-            System.out.println("Snake option");
-            currentPosition = currentPosition - diceNum;
+            int diceNum = (int) (Math.random() * 10) % 6 + 1;
+            int option = (int) (Math.random() * 10) % 3;
+
+            if (option == IS_SNAKE) {
+                currentPosition = currentPosition - diceNum;
+                if (currentPosition < 0) {
+                    currentPosition = START_POSITIION;
+                    System.out.println("Snake option and player is at " + currentPosition);
+                } else {
+                   // currentPosition = currentPosition - diceNum;
+                System.out.println("Snake option and player is at " + currentPosition);
+                }
+
+            } else if (option == IS_LADDER) {
+                currentPosition = currentPosition + diceNum;
+                System.out.println("Ladder option and player is at "+currentPosition);
+            } else {
+                System.out.println("No Play option and player is at "+currentPosition);
+            }
         }
-        else if(option==IS_LADDER){
-            System.out.println("Ladder option");
-            currentPosition = currentPosition + diceNum;
-        }
-        else{
-            System.out.println("No Play option");
-        }
-        System.out.println("Player is at "+currentPosition+" position");
+        System.out.println("Player won at "+currentPosition+" position");
     }
 }
