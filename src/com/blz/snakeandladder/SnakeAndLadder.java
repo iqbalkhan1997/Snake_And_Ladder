f594e71b3aebe00f056da356d5d3582368709ac3
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-23 14:03:28 +0530

    Added: options of NoPlay,Snake and Ladder functionality

--- src/com/blz/snakeandladder/SnakeAndLadder.java
+++ src/com/blz/snakeandladder/SnakeAndLadder.java
@@ -1,12 +1,30 @@
 package com.blz.snakeandladder;
 
 public class SnakeAndLadder {
+      //  public static final int START_POSITIION=0;
+    public static final int IS_SNAKE=1;
+    public static final int IS_LADDER=2;
 
     public static void main(String[] args) {
             System.out.println("Welcome to Snake and Ladder Game");
-            int startPosition=0;
-        System.out.println("Player is at "+startPosition+" position");
+            int currentPosition=0;
+
+        System.out.println("Player is at "+currentPosition+" position");
         int diceNum=(int)(Math.random() * 10) % 6 + 1;
         System.out.println("die value is "+diceNum);
+        int option=(int)(Math.random() * 10) % 3;
+
+        if(option==IS_SNAKE){
+            System.out.println("Snake option");
+            currentPosition = currentPosition - diceNum;
+        }
+        else if(option==IS_LADDER){
+            System.out.println("Ladder option");
+            currentPosition = currentPosition + diceNum;
+        }
+        else{
+            System.out.println("No Play option");
+        }
+        System.out.println("Player is at "+currentPosition+" position");
     }
 }
